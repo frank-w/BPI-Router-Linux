9d976e15095d9f5ff1eeef877a9cf8e24b0a6890
Ryder Lee <ryder.lee@mediatek.com>
2018-11-16 16:33:00 +0100

    [hdmi] fix possible_crtcs
    source: http://forum.banana-pi.org/t/kernel-4-19-rc1-for-testers/6618/52

--- drivers/gpu/drm/mediatek/mtk_dpi.c
+++ drivers/gpu/drm/mediatek/mtk_dpi.c
@@ -610,7 +610,7 @@ static int mtk_dpi_bind(struct device *dev, struct device *master, void *data)
 	drm_encoder_helper_add(&dpi->encoder, &mtk_dpi_encoder_helper_funcs);
 
 	/* Currently DPI0 is fixed to be driven by OVL1 */
-	dpi->encoder.possible_crtcs = BIT(1);
+	dpi->encoder.possible_crtcs = BIT(0)|BIT(1);
 
 	ret = drm_bridge_attach(&dpi->encoder, dpi->bridge, NULL);
 	if (ret) {
