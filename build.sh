40da332c05db44d29c4690f2171c52358a42ac56
Frank Wunderlich <frank-w@public-files.de>
2019-03-06 19:02:42 +0100

    [build.sh] fix travis-build with cryptodev

--- build.sh
+++ build.sh
@@ -503,7 +503,7 @@ if [ -n "$kernver" ]; then
 		"build")
 			echo "Build Kernel"
 			build
-			#$0 cryptodev
+			$0 cryptodev
 			;;
 
 		"spidev")
@@ -546,7 +546,7 @@ if [ -n "$kernver" ]; then
 				exit 1;
 			fi;
 			$0 build
-			$0 cryptodev
+			#$0 cryptodev
 			if [ -e "./uImage" ]; then
 				echo "==========================================="
 				echo "1) pack"
