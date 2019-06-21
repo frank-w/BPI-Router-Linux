474203bc4de46f22dd774fbb60c7734288ea7931
René van Dorst <opensource@vdorst.com>
2019-06-21 10:04:05 +0200

    net: ethernet: mediatek: support net-labels
    With this patch, device name can be set within dts file in the same way as dsa
    port can.
    Add: label = "wan"; to GMAC node.
    
    Signed-off-by: René van Dorst <opensource@vdorst.com>

--- drivers/net/ethernet/mediatek/mtk_eth_soc.c
+++ drivers/net/ethernet/mediatek/mtk_eth_soc.c
@@ -2801,6 +2801,7 @@ static const struct net_device_ops mtk_netdev_ops = {
 
 static int mtk_add_mac(struct mtk_eth *eth, struct device_node *np)
 {
+	const char *name = of_get_property(np, "label", NULL);
 	const __be32 *_id = of_get_property(np, "reg", NULL);
 	phy_interface_t phy_mode;
 	struct phylink *phylink;
@@ -2890,6 +2891,9 @@ static int mtk_add_mac(struct mtk_eth *eth, struct device_node *np)
 
 	eth->netdev[id]->irq = eth->irq[0];
 	eth->netdev[id]->dev.of_node = np;
+
+	if (name)
+		strlcpy(eth->netdev[id]->name, name, IFNAMSIZ);
 
 	return 0;
 
