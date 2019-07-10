62d3840cdd646707a3aa003a3906e1565fe08daf
Martin Sommer <martin.sommer@staffbase.com>
2019-07-10 19:36:00 +0200

    bug: silently ignore existing SD folder

--- docker/run.sh
+++ docker/run.sh
@@ -3,5 +3,5 @@
 DIR=$(dirname "$(readlink -f "$0")")
 REPO_DIR="$DIR/.."
 
-mkdir "$REPO_DIR/SD"
+mkdir -p "$REPO_DIR/SD"
 docker run -it -v "$REPO_DIR/SD":/SD -v "$REPO_DIR":/srv bpi-cross-compile:1 /bin/bash
