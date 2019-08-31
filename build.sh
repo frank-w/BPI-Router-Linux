bdae1a9a4b11083ace2c0ed3989f913996a85a77
Frank Wunderlich <frank-w@public-files.de>
2019-08-31 17:27:28 +0200

    made a simple build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make mt7623n_evb_bpi_defconfig
-ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make INSTALL_MOD_PATH=output UIMAGE_LOADADDR=0x80008000 uImage dtbs
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export INSTALL_MOD_PATH=output
+
+case $1 in
+	importconfig) make mt7623n_evb_bpi_defconfig;;
+	config) make menuconfig;;
+	build) make UIMAGE_LOADADDR=0x80008000 uImage dtbs modules modules_install;;
+	*) echo "build.sh [importconfig|config|build]";;
+esac
