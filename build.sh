e40e9c35e877a83a2bb4ae37815bcda9df812c5d
Frank Wunderlich <frank-w@public-files.de>
2019-08-31 16:37:47 +0200

    remove temporary defconfig and change build-script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make mt7623_bpi-r2_defconfig
-ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make
+ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make mt7623n_evb_bpi_defconfig
+ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make INSTALL_MOD_PATH=output UIMAGE_LOADADDR=0x80008000 uImage dtbs
