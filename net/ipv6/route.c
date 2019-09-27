c57aba65c830b143377f5925f0f20bf5237358c1
Frank Wunderlich <frank-w@public-files.de>
2019-09-27 19:55:04 +0200

    fix build error

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -1424,7 +1424,7 @@ void ip6_sk_update_pmtu(struct sk_buff *skb, struct sock *sk, __be32 mtu)
 	if (!oif && skb->dev)
 		oif = l3mdev_master_ifindex(skb->dev);
 
-	ip6_update_pmtu(skb, sock_net(sk), mtu, oif, sk->sk_mark);
+	ip6_update_pmtu(skb, sock_net(sk), mtu, oif, sk->sk_mark, sk->sk_uid);
 }
 EXPORT_SYMBOL_GPL(ip6_sk_update_pmtu);
 
