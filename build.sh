c36a67258a83525a79cae07f8fd9369c27d2a46d
Frank Wunderlich <frank-w@public-files.de>
2019-09-27 20:00:44 +0200

    build.sh: enable log and multithreading

--- build.sh
+++ build.sh
@@ -2,10 +2,13 @@
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
 export INSTALL_MOD_PATH=output
+CFLAGS=-j$(grep ^processor /proc/cpuinfo  | wc -l)
 
 case $1 in
 	importconfig) make mt7623n_evb_bpi_defconfig;;
 	config) make menuconfig;;
-	build) make UIMAGE_LOADADDR=0x80008000 uImage dtbs modules modules_install;;
+	build) make ${CFLAGS} UIMAGE_LOADADDR=0x80008000 uImage dtbs modules 2> >(tee build.log) &&
+		make modules_install
+	;;
 	*) echo "build.sh [importconfig|config|build]";;
 esac
