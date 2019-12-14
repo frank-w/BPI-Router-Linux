45be0aec7fd25ea15ce59ec7c0d4061670cdf517
Frank Wunderlich <frank-w@public-files.de>
2019-12-14 14:47:51 +0100

    mt6625l: add changes outside driver dir
    - without watchdog-changes because toprgu seems to be added with
      c254e103082b ("watchdog: mtk_wdt: mt8183: Add reset controller)
    - mt6625l: watchdog: implement data for mt6589
    - timekeeping32: reverting changes for wifi-driver
    - dts: mt7623: reorder btif
    - fix missing sched_setscheduler export

--- drivers/watchdog/mtk_wdt.c
+++ drivers/watchdog/mtk_wdt.c
@@ -74,6 +74,10 @@ static const struct mtk_wdt_data mt2712_data = {
 	.toprgu_sw_rst_num = MT2712_TOPRGU_SW_RST_NUM,
 };
 
+static const struct mtk_wdt_data mt6589_data = {
+	.toprgu_sw_rst_num = 15,
+};
+
 static const struct mtk_wdt_data mt8183_data = {
 	.toprgu_sw_rst_num = MT8183_TOPRGU_SW_RST_NUM,
 };
@@ -405,7 +409,7 @@ static int mtk_wdt_resume(struct device *dev)
 
 static const struct of_device_id mtk_wdt_dt_ids[] = {
 	{ .compatible = "mediatek,mt2712-wdt", .data = &mt2712_data },
-	{ .compatible = "mediatek,mt6589-wdt" },
+	{ .compatible = "mediatek,mt6589-wdt", .data = &mt6589_data },
 	{ .compatible = "mediatek,mt8183-wdt", .data = &mt8183_data },
 	{ .compatible = "mediatek,mt8192-wdt", .data = &mt8192_data },
 	{ /* sentinel */ }
