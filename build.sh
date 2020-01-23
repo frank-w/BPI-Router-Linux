4ad8f399c829218843839ef0eb49c587e09a3880
Frank Wunderlich <frank-w@public-files.de>
2020-01-23 18:45:06 +0100

    build.sh: add install (to output-dir)

--- build.sh
+++ build.sh
@@ -5,10 +5,12 @@ export INSTALL_MOD_PATH=output
 CFLAGS=-j$(grep ^processor /proc/cpuinfo  | wc -l)
 
 case $1 in
-	importconfig) make mt7623n_evb_bpi_defconfig;;
-	config) make menuconfig;;
-	build) make ${CFLAGS} UIMAGE_LOADADDR=0x80008000 uImage dtbs modules 2> >(tee build.log) &&
+	"importconfig") make mt7623n_evb_bpi_defconfig;;
+	"config") make menuconfig;;
+	"build") make ${CFLAGS} UIMAGE_LOADADDR=0x80008000 uImage dtbs modules 2> >(tee build.log);;
+	"install")
+		cp arch/arm/boot/uImage $INSTALL_MOD_PATH/uImage_$(make kernelversion)
 		make modules_install
 	;;
-	*) echo "build.sh [importconfig|config|build]";;
+	*) echo "build.sh [importconfig|config|build|install]";;
 esac
