b6cc379dde4c64c21db7c8544ebc595492c48d92
Frank Wunderlich <frank-w@public-files.de>
2020-08-01 10:02:34 +0200

    build.sh: add upload-option (for tftp)

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ export CROSS_COMPILE=arm-linux-gnueabihf-
 export INSTALL_MOD_PATH=output
 CFLAGS=-j$(grep ^processor /proc/cpuinfo  | wc -l)
 
+uploaduser=$USER
+uploadserver=192.168.0.10
+uploaddir=/var/lib/tftp
+
 case $1 in
 	"importconfig") make mt7623n_evb_bpi_defconfig;;
 	"config") make menuconfig;;
@@ -12,5 +16,9 @@ case $1 in
 		cp arch/arm/boot/uImage $INSTALL_MOD_PATH/uImage_$(make kernelversion)
 		make modules_install
 	;;
+	"upload")
+		targetfile=uImage_$(make kernelversion)
+		echo "uploading $targetfile to ${uploadserver}..."
+		scp arch/arm/boot/uImage ${uploaduser}@${uploadserver}:/${uploaddir}/${targetfile};;
 	*) echo "build.sh [importconfig|config|build|install]";;
 esac
