626bbe644a789f37cae47c97bb2e2809368b3aca
John Thomson <git@johnthomson.fastmail.com.au>
2020-12-25 18:50:08 +1000

    mtd: spi-nor: write support for minor aligned partitions
    Do not prevent writing to mtd partitions where a partition boundary sits
    on a minor erasesize boundary.
    This addresses a FIXME that has been present since the start of the
    linux git history:
    /* Doesn't start on a boundary of major erase size */
    /* FIXME: Let it be writable if it is on a boundary of
     * _minor_ erase size though */
    
    Allow a uniform erase region spi-nor device to be configured
    to use the non-uniform erase regions code path for an erase with:
    CONFIG_MTD_SPI_NOR_USE_VARIABLE_ERASE=y
    
    On supporting hardware (SECT_4K: majority of current SPI-NOR device)
    provide the facility for an erase to use the least number
    of SPI-NOR operations, as well as access to 4K erase without
    requiring CONFIG_MTD_SPI_NOR_USE_4K_SECTORS
    
    Introduce erasesize_minor to the mtd struct,
    the smallest erasesize supported by the device
    
    On existing devices, this is useful where write support is wanted
    for data on a 4K partition, such as some u-boot-env partitions,
    or RouterBoot soft_config, while still netting the performance
    benefits of using 64K sectors
    
    Performance:
    time mtd erase firmware
    OpenWrt 5.10 ramips MT7621 w25q128jv 0xfc0000 partition length
    
    Without this patch
    MTD_SPI_NOR_USE_4K_SECTORS=y	|n
    real    2m 11.66s		|0m 50.86s
    user    0m 0.00s		|0m 0.00s
    sys     1m 56.20s		|0m 50.80s
    
    With this patch
    MTD_SPI_NOR_USE_VARIABLE_ERASE=n|y		|4K_SECTORS=y
    real    0m 51.68s		|0m 50.85s	|2m 12.89s
    user    0m 0.00s		|0m 0.00s	|0m 0.01s
    sys     0m 46.94s		|0m 50.38s	|2m 12.46s
    
    Signed-off-by: John Thomson <git@johnthomson.fastmail.com.au>
    Signed-off-by: Thibaut VARÈNE <hacks+kernel@slashdirt.org>

--- drivers/mtd/spi-nor/core.c
+++ drivers/mtd/spi-nor/core.c
@@ -1149,6 +1149,8 @@ static u8 spi_nor_convert_3to4_erase(u8 opcode)
 
 static bool spi_nor_has_uniform_erase(const struct spi_nor *nor)
 {
+	if (IS_ENABLED(CONFIG_MTD_SPI_NOR_USE_VARIABLE_ERASE))
+		return false;
 	return !!nor->params->erase_map.uniform_erase_type;
 }
 
@@ -2581,6 +2583,7 @@ static int spi_nor_select_erase(struct spi_nor *nor)
 {
 	struct spi_nor_erase_map *map = &nor->params->erase_map;
 	const struct spi_nor_erase_type *erase = NULL;
+	const struct spi_nor_erase_type *erase_minor = NULL;
 	struct mtd_info *mtd = &nor->mtd;
 	u32 wanted_size = nor->info->sector_size;
 	int i;
@@ -2613,8 +2616,9 @@ static int spi_nor_select_erase(struct spi_nor *nor)
 	 */
 	for (i = SNOR_ERASE_TYPE_MAX - 1; i >= 0; i--) {
 		if (map->erase_type[i].size) {
-			erase = &map->erase_type[i];
-			break;
+			if (!erase)
+				erase = &map->erase_type[i];
+			erase_minor = &map->erase_type[i];
 		}
 	}
 
@@ -2622,6 +2626,9 @@ static int spi_nor_select_erase(struct spi_nor *nor)
 		return -EINVAL;
 
 	mtd->erasesize = erase->size;
+	if (IS_ENABLED(CONFIG_MTD_SPI_NOR_USE_VARIABLE_ERASE) &&
+			erase_minor && erase_minor->size < erase->size)
+		mtd->erasesize_minor = erase_minor->size;
 	return 0;
 }
 
