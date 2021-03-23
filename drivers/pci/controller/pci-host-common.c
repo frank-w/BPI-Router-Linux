986e8a33fea5db58f1621fce28f1ec5d57efd7d6
Frank Wunderlich <frank-w@public-files.de>
2021-03-23 09:43:49 +0100

    Revert "pci: fix MSI issue part 2"
    This reverts commit aae45a226341c84a12b431abb7904bb39cac53c2.

--- drivers/pci/controller/pci-host-common.c
+++ drivers/pci/controller/pci-host-common.c
@@ -77,7 +77,6 @@ int pci_host_common_probe(struct platform_device *pdev)
 
 	bridge->sysdata = cfg;
 	bridge->ops = (struct pci_ops *)&ops->pci_ops;
-	bridge->msi_domain = true;
 
 	platform_set_drvdata(pdev, bridge);
 
