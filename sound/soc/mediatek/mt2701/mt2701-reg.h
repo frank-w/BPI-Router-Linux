9883eaa1fba4c5b9aa472ad796d87b17b47f6594
Alex Ryabchenko <d3adme4t@gmail.com>
2021-05-10 00:18:43 -0700

    HDMI related registers ported from 5.9-hdmisnd

--- sound/soc/mediatek/mt2701/mt2701-reg.h
+++ sound/soc/mediatek/mt2701/mt2701-reg.h
@@ -90,6 +90,21 @@
 #define AFE_DAI_BASE 0x1370
 #define AFE_DAI_CUR 0x137c
 
+#define AFE_HDMI_OUT_CON0	0x0370
+#define AFE_HDMI_OUT_BASE	0x0374
+#define AFE_HDMI_OUT_CUR	0x0378
+#define AFE_HDMI_OUT_END	0x037c
+#define AFE_HDMI_CONN0		0x0390
+
+#define AFE_IRQ_MCU_CON		0x03a0
+#define AFE_IRQ_STATUS		0x03a4
+#define AFE_IRQ_CLR            0x03a8
+#define AFE_IRQ_CNT1		0x03ac
+#define AFE_IRQ_CNT2		0x03b0
+#define AFE_IRQ_MCU_EN		0x03b4
+#define AFE_IRQ_CNT5		0x03bc
+#define AFE_IRQ_CNT7		0x03dc
+
 /* AFE_DAIBT_CON0 (0x001c) */
 #define AFE_DAIBT_CON0_DAIBT_EN		(0x1 << 0)
 #define AFE_DAIBT_CON0_BT_FUNC_EN	(0x1 << 1)
@@ -114,6 +129,108 @@
 
 /* AFE_DAC_CON0 (0x1200) */
 #define AFE_DAC_CON0_AFE_ON		(0x1 << 0)
+
+/* AUDIO_TOP_CON3 */
+#define HDMI_OUT_SPEAKER_BIT    4
+#define SPEAKER_OUT_HDMI        5
+#define HDMI_2CH_SEL_POS        6
+#define HDMI_2CH_SEL_LEN        2
+
+/* AFE_HDMI_OUT_CON0 */
+#define HDMI_OUT_ON_MASK        1
+#define HDMI_OUT_ON         1
+#define HDMI_OUT_OFF        0
+#define HDMI_OUT_BIT_WIDTH_MASK      (1 << 1)
+#define HDMI_OUT_BIT_WIDTH_16     0
+#define HDMI_OUT_BIT_WIDTH_32     (1 << 1)
+#define HDMI_OUT_DSD_WDITH_MASK      (0x3 << 2)
+#define HDMI_OUT_DSD_32BIT       (0x0 << 2)
+#define HDMI_OUT_DSD_16BIT       (0x1 << 2)
+#define HDMI_OUT_DSD_8BIT        (0x2 << 2)
+#define HDMI_OUT_CH_NUM_MASK     (0xF << 4)
+#define HDMI_OUT_CH_NUM_POS      4
+
+/* AFE_HDMI_CONN0 */
+#define SPDIF_LRCK_SRC_SEL_MASK  (1 << 30)
+#define SPDIF_LRCK_SRC_SEL_SPDIF 0
+#define SPDIF_LRCK_SRC_SEL_HDMI  (1 << 30)
+#define SPDIF2_LRCK_SRC_SEL_MASK  (1 << 31)
+#define SPDIF2_LRCK_SRC_SEL_SPDIF 0
+#define SPDIF2_LRCK_SRC_SEL_HDMI  (1 << 31)
+
+/* AFE_8CH_I2S_OUT_CON */
+#define HDMI_8CH_I2S_CON_EN_MASK 1
+#define HDMI_8CH_I2S_CON_EN  1
+#define HDMI_8CH_I2S_CON_DIS 0
+
+#define HDMI_8CH_I2S_CON_BCK_INV_MASK   (1 << 1)
+#define HDMI_8CH_I2S_CON_BCK_INV              (1 << 1)
+#define HDMI_8CH_I2S_CON_BCK_NO_INV  0
+
+#define HDMI_8CH_I2S_CON_LRCK_INV_MASK   (1 << 2)
+#define HDMI_8CH_I2S_CON_LRCK_INV    (1 << 2)
+#define HDMI_8CH_I2S_CON_LRCK_NO_INV 0
+
+#define HDMI_8CH_I2S_CON_I2S_DELAY_MASK    (1 << 3)
+#define HDMI_8CH_I2S_CON_I2S_DELAY     (1 << 3)
+#define HDMI_8CH_I2S_CON_I2S_NO_DELAY  0
+
+#define HDMI_8CH_I2S_CON_I2S_WLEN_MASK     (3 << 4)
+#define HDMI_8CH_I2S_CON_I2S_8BIT      0
+#define HDMI_8CH_I2S_CON_I2S_16BIT     (1 << 4)
+#define HDMI_8CH_I2S_CON_I2S_24BIT     (2 << 4)
+#define HDMI_8CH_I2S_CON_I2S_32BIT     (3 << 4)
+
+#define HDMI_8CH_I2S_CON_DSD_MODE_MASK     (0x1 << 6)
+#define HDMI_8CH_I2S_CON_DSD           (0x1 << 6)
+#define HDMI_8CH_I2S_CON_NON_DSD       (0x0 << 6)
+/* AFE_SIDETONE_DEBUG */
+#define STF_SRC_SEL_POS     16
+#define STF_SRC_SEL_LEN     2
+#define STF_I5I6_SEL_POS    19
+#define STF_I5I6_SEL_LEN    1
+
+/* AFE_SIDETONE_CON0 */
+#define STF_COEFF_VAL_POS       0
+#define STF_COEFF_VAL_LEN       16
+#define STF_COEFF_ADDRESS_POS   16
+#define STF_COEFF_ADDRESS_LEN   5
+#define STF_CH_SEL_POS          23
+#define STF_CH_SEL_LEN          1
+#define STF_COEFF_W_ENABLE_POS  24
+#define STF_COEFF_W_ENABLE_LEN  1
+#define STF_W_ENABLE_POS        25
+#define STF_W_ENABLE_LEN        1
+#define STF_COEFF_BIT       0x0000FFFF
+
+/* AFE_SIDETONE_CON1 */
+#define STF_TAP_NUM_POS 0
+#define STF_TAP_NUM_LEN	5
+#define STF_ON_POS      8
+#define STF_ON_LEN      1
+#define STF_BYPASS_POS  31
+#define STF_BYPASS_LEN  1
+
+/* AFE_SGEN_CON0 */
+#define SINE_TONE_FREQ_DIV_CH1  0
+#define SINE_TONE_AMP_DIV_CH1   5
+#define SINE_TONE_MODE_CH1      8
+#define SINE_TONE_FREQ_DIV_CH2  12
+#define SINE_TONE_AMP_DIV_CH2   17
+#define SINE_TONE_MODE_CH2      20
+#define SINE_TONE_MUTE_CH1      24
+#define SINE_TONE_MUTE_CH2      25
+#define SINE_TONE_ENABLE        26
+#define SINE_TONE_LOOPBACK_MOD  28
+
+
+/* AFE_SPDIF_OUT_CON0 */
+#define SPDIF_OUT_CLOCK_ON_OFF_MASK  1
+#define SPDIF_OUT_CLOCK_ON       1
+#define SPDIF_OUT_CLOCK_OFF      0
+#define SPDIF_OUT_MEMIF_ON_OFF_MASK  (1 << 1)
+#define SPDIF_OUT_MEMIF_ON       (1 << 1)
+#define SPDIF_OUT_MEMIF_OFF      0
 
 /* AFE_MEMIF_PBUF_SIZE (0x1238) */
 #define AFE_MEMIF_PBUF_SIZE_DLM_MASK		(0x1 << 29)
