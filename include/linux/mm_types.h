f24427b39df7de6de17f6953c67edad116d68bd5
Dan Williams <dan.j.williams@intel.com>
2022-10-14 16:58:49 -0700

    devdax: sparse fixes for vm_fault_t in tracepoints
    Now that the dax-mapping-entry code has moved to a common location take
    the opportunity to fixup some long standing sparse warnings.  In this case
    the tracepoints have long specified the wrong type for the traced return
    code.  Pass the correct type, but handle casting it back to 'unsigned int'
    inside the trace helpers as the helpers are not prepared to handle
    restricted types.
    
    Link: https://lkml.kernel.org/r/166579192919.2236710.12464252412504907962.stgit@dwillia2-xfh.jf.intel.com
    Fixes:
    drivers/dax/mapping.c:1031:55: sparse: warning: incorrect type in argument 3 (different base types)
    drivers/dax/mapping.c:1031:55: sparse:    expected int result
    drivers/dax/mapping.c:1031:55: sparse:    got restricted vm_fault_t
    drivers/dax/mapping.c:1046:58: sparse: warning: incorrect type in argument 3 (different base types)
    drivers/dax/mapping.c:1046:58: sparse:    expected int result
    drivers/dax/mapping.c:1046:58: sparse:    got restricted vm_fault_t [assigned] [usertype] ret
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Reported-by: kernel test robot <lkp@intel.com>
      Link: http://lore.kernel.org/r/202210091141.cHaQEuCs-lkp@intel.com
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: Alistair Popple <apopple@nvidia.com>
    Cc: Ben Skeggs <bskeggs@redhat.com>
    Cc: "Christian König" <christian.koenig@amd.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: "Darrick J. Wong" <djwong@kernel.org>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Felix Kuehling <Felix.Kuehling@amd.com>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Jason Gunthorpe <jgg@nvidia.com>
    Cc: Jerome Glisse <jglisse@redhat.com>
    Cc: John Hubbard <jhubbard@nvidia.com>
    Cc: Karol Herbst <kherbst@redhat.com>
    Cc: Lyude Paul <lyude@redhat.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: "Pan, Xinhui" <Xinhui.Pan@amd.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -905,19 +905,19 @@ enum vm_fault_reason {
 			VM_FAULT_HWPOISON_LARGE | VM_FAULT_FALLBACK)
 
 #define VM_FAULT_RESULT_TRACE \
-	{ VM_FAULT_OOM,                 "OOM" },	\
-	{ VM_FAULT_SIGBUS,              "SIGBUS" },	\
-	{ VM_FAULT_MAJOR,               "MAJOR" },	\
-	{ VM_FAULT_WRITE,               "WRITE" },	\
-	{ VM_FAULT_HWPOISON,            "HWPOISON" },	\
-	{ VM_FAULT_HWPOISON_LARGE,      "HWPOISON_LARGE" },	\
-	{ VM_FAULT_SIGSEGV,             "SIGSEGV" },	\
-	{ VM_FAULT_NOPAGE,              "NOPAGE" },	\
-	{ VM_FAULT_LOCKED,              "LOCKED" },	\
-	{ VM_FAULT_RETRY,               "RETRY" },	\
-	{ VM_FAULT_FALLBACK,            "FALLBACK" },	\
-	{ VM_FAULT_DONE_COW,            "DONE_COW" },	\
-	{ VM_FAULT_NEEDDSYNC,           "NEEDDSYNC" }
+	{ (__force unsigned int) VM_FAULT_OOM,                 "OOM" },	\
+	{ (__force unsigned int) VM_FAULT_SIGBUS,              "SIGBUS" },	\
+	{ (__force unsigned int) VM_FAULT_MAJOR,               "MAJOR" },	\
+	{ (__force unsigned int) VM_FAULT_WRITE,               "WRITE" },	\
+	{ (__force unsigned int) VM_FAULT_HWPOISON,            "HWPOISON" },	\
+	{ (__force unsigned int) VM_FAULT_HWPOISON_LARGE,      "HWPOISON_LARGE" },	\
+	{ (__force unsigned int) VM_FAULT_SIGSEGV,             "SIGSEGV" },	\
+	{ (__force unsigned int) VM_FAULT_NOPAGE,              "NOPAGE" },	\
+	{ (__force unsigned int) VM_FAULT_LOCKED,              "LOCKED" },	\
+	{ (__force unsigned int) VM_FAULT_RETRY,               "RETRY" },	\
+	{ (__force unsigned int) VM_FAULT_FALLBACK,            "FALLBACK" },	\
+	{ (__force unsigned int) VM_FAULT_DONE_COW,            "DONE_COW" },	\
+	{ (__force unsigned int) VM_FAULT_NEEDDSYNC,           "NEEDDSYNC" }
 
 struct vm_special_mapping {
 	const char *name;	/* The name, e.g. "[vdso]". */
