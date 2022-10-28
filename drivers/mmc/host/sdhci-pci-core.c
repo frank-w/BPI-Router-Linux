208d4efa941286c67f2bb1cb3e8dd1d0ddda3de6
Brian Norris <briannorris@chromium.org>
2022-10-28 15:56:37 -0700

    mmc: sdhci-pci: Set PROBE_PREFER_ASYNCHRONOUS
    This driver often takes on the order of 10ms to start, but in some cases
    as much as 190ms. It shouldn't have many cross-device dependencies to
    race with, nor racy access to shared state with other drivers, so this
    should be a relatively low risk change. We've done similarly with a
    variety of other MMC host drivers already.
    
    This driver was pinpointed as part of a survey of top slowest initcalls
    (i.e., are built in, and probing synchronously) on a lab of ChromeOS
    systems.
    
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Link: https://lore.kernel.org/r/20221028155633.1.I6c4bfb31e88fad934e7360242cb662e01612c1bb@changeid
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sdhci-pci-core.c
+++ drivers/mmc/host/sdhci-pci-core.c
@@ -2274,7 +2274,8 @@ static struct pci_driver sdhci_driver = {
 	.probe =	sdhci_pci_probe,
 	.remove =	sdhci_pci_remove,
 	.driver =	{
-		.pm =   &sdhci_pci_pm_ops
+		.pm =   &sdhci_pci_pm_ops,
+		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
 	},
 };
 
