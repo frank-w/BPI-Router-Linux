5744534bdae403270f86d934a6c5878da6a5a64f
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2022-11-09 19:51:29 +0300

    x86/uaccess: Provide untagged_addr() and remove tags before address check
    untagged_addr() is a helper used by the core-mm to strip tag bits and
    get the address to the canonical shape. In only handles userspace
    addresses. The untagging mask is stored in mmu_context and will be set
    on enabling LAM for the process.
    
    The tags must not be included into check whether it's okay to access the
    userspace address.
    
    Strip tags in access_ok().
    
    get_user() and put_user() don't use access_ok(), but check access
    against TASK_SIZE directly in assembly. Strip tags, before calling into
    the assembly helper.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Andy Lutomirski <luto@kernel.org>
    Tested-by: Alexander Potapenko <glider@google.com>
    Link: https://lore.kernel.org/all/20221109165140.9137-6-kirill.shutemov%40linux.intel.com

--- arch/x86/include/asm/mmu.h
+++ arch/x86/include/asm/mmu.h
@@ -43,6 +43,9 @@ typedef struct {
 
 	/* Active LAM mode:  X86_CR3_LAM_U48 or X86_CR3_LAM_U57 or 0 (disabled) */
 	unsigned long lam_cr3_mask;
+
+	/* Significant bits of the virtual address. Excludes tag bits. */
+	u64 untag_mask;
 #endif
 
 	struct mutex lock;
--- arch/x86/kernel/process.c
+++ arch/x86/kernel/process.c
@@ -47,6 +47,7 @@
 #include <asm/frame.h>
 #include <asm/unwind.h>
 #include <asm/tdx.h>
+#include <asm/mmu_context.h>
 
 #include "process.h"
 
@@ -367,6 +368,8 @@ void arch_setup_new_exec(void)
 		task_clear_spec_ssb_noexec(current);
 		speculation_ctrl_update(read_thread_flags());
 	}
+
+	mm_reset_untag_mask(current->mm);
 }
 
 #ifdef CONFIG_X86_IOPL_IOPERM
