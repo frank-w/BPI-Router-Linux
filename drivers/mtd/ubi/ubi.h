346dcd181dd4f7f2f43d7759f5e83d251aff2781
Daniel Golle <daniel@makrotopia.org>
2022-12-22 18:36:55 +0000

    mtd: ubi: attach MTD partition from device-tree
    Split ubi_init() function into early function to be called by
    device_initcall() and keep cmdline attachment in late_initcall().
    (when building ubi as module, both is still done in a single
    module_init() call)
    
    Register MTD notifier and attach MTD devices which are marked as
    compatible with 'linux,ubi' in OF device-tree when being added, detach
    UBI device from MTD device when it is being removed.
    
    For existing users this should not change anything besides automatic
    removal of (dead) UBI devices when their underlying MTD devices are
    already gone, e.g. in case of MTD driver module or (SPI) bus driver
    module being removed.
    
    For new users this opens up the option to attach UBI using device-tree
    which then happens early and in parallel with other drivers being
    probed which slightly reduces the total boot time.
    
    Attachment no longer happening late is also a requirement for other
    drivers to make use of UBI, e.g. drivers/nvmem/u-boot-env.c can now
    be extended to support U-Boot environment stored in UBI volumes.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- drivers/mtd/ubi/ubi.h
+++ drivers/mtd/ubi/ubi.h
@@ -939,7 +939,7 @@ int ubi_io_write_vid_hdr(struct ubi_device *ubi, int pnum,
 int ubi_attach_mtd_dev(struct mtd_info *mtd, int ubi_num,
 		       int vid_hdr_offset, int max_beb_per1024,
 		       bool disable_fm);
-int ubi_detach_mtd_dev(int ubi_num, int anyway);
+int ubi_detach_mtd_dev(int ubi_num, int anyway, bool have_lock);
 struct ubi_device *ubi_get_device(int ubi_num);
 void ubi_put_device(struct ubi_device *ubi);
 struct ubi_device *ubi_get_by_major(int major);
