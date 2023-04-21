93497a455f187ebd1f770f330219a0318470443d
Gabriel Krisman Bertazi <krisman@collabora.com>
2023-04-21 19:56:14 -0400

    fs: Add DCACHE_CASEFOLDED_NAME flag
    This flag marks a negative or positive dentry as being created after a
    case-insensitive lookup operation.  It is useful to differentiate
    dentries this way to detect whether the negative dentry can be trusted
    during a case-insensitive lookup.
    
    Signed-off-by: Gabriel Krisman Bertazi <krisman@collabora.com>
    Reviewed-by: Theodore Ts'o <tytso@mit.edu>
    Reviewed-by: Eric Biggers <ebiggers@google.com>
    ---
    Changes since v5:
      - Fix EXPORT_SYMBOL (Eric, kernel test bot)
    Changes since v4:
      - Fixup names of functions to reflect flag name change (Eric)
    
    Changes since v2:
      -  Rename DCACHE_CASEFOLD_LOOKUP -> DCACHE_CASEFOLDED_NAME (Eric)

--- include/linux/dcache.h
+++ include/linux/dcache.h
@@ -208,6 +208,7 @@ struct dentry_operations {
 #define DCACHE_FALLTHRU			0x01000000 /* Fall through to lower layer */
 #define DCACHE_NOKEY_NAME		0x02000000 /* Encrypted name encoded without key */
 #define DCACHE_OP_REAL			0x04000000
+#define DCACHE_CASEFOLDED_NAME		0x08000000 /* Dentry comes from a casefold directory */
 
 #define DCACHE_PAR_LOOKUP		0x10000000 /* being looked up (with parent locked shared) */
 #define DCACHE_DENTRY_CURSOR		0x20000000
@@ -494,6 +495,13 @@ extern void d_set_fallthru(struct dentry *dentry);
 static inline bool d_is_fallthru(const struct dentry *dentry)
 {
 	return dentry->d_flags & DCACHE_FALLTHRU;
+}
+
+extern void d_set_casefolded_name(struct dentry *dentry);
+
+static inline bool d_is_casefolded_name(const struct dentry *dentry)
+{
+	return dentry->d_flags & DCACHE_CASEFOLDED_NAME;
 }
 
 
