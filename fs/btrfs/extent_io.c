9ba11971c721ba96363fc6a2c2ba22c24381ce8a
Christoph Hellwig <hch@lst.de>
2023-07-24 06:26:54 -0700

    btrfs: don't wait for writeback on clean pages in extent_write_cache_pages
    __extent_writepage could have started on more pages than the one it was
    called for.  This happens regularly for zoned file systems, and in theory
    could happen for compressed I/O if the worker thread was executed very
    quickly. For such pages extent_write_cache_pages waits for writeback
    to complete before moving on to the next page, which is highly inefficient
    as it blocks the flusher thread.
    
    Port over the PageDirty check that was added to write_cache_pages in
    commit 515f4a037fb ("mm: write_cache_pages optimise page cleaning") to
    fix this.
    
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -2075,6 +2075,12 @@ retry:
 				continue;
 			}
 
+			if (!folio_test_dirty(folio)) {
+				/* Someone wrote it for us. */
+				folio_unlock(folio);
+				continue;
+			}
+
 			if (wbc->sync_mode != WB_SYNC_NONE) {
 				if (folio_test_writeback(folio))
 					submit_write_bio(bio_ctrl, 0);
