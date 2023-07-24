95f86918290a6b761fe0832c5b63d80019da44f9
Matthew Wilcox (Oracle) <willy@infradead.org>
2023-07-24 19:54:01 +0100

    mm: remove CONFIG_PER_VMA_LOCK ifdefs
    Patch series "Handle most file-backed faults under the VMA lock", v3.
    
    This patchset adds the ability to handle page faults on parts of files
    which are already in the page cache without taking the mmap lock.
    
    
    This patch (of 10):
    
    Provide lock_vma_under_rcu() when CONFIG_PER_VMA_LOCK is not defined to
    eliminate ifdefs in the users.
    
    Link: https://lkml.kernel.org/r/20230724185410.1124082-1-willy@infradead.org
    Link: https://lkml.kernel.org/r/20230724185410.1124082-2-willy@infradead.org
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: Suren Baghdasaryan <surenb@google.com>
    Cc: Punit Agrawal <punit.agrawal@bytedance.com>
    Cc: Arjun Roy <arjunroy@google.com>
    Cc: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -1328,7 +1328,6 @@ void do_user_addr_fault(struct pt_regs *regs,
 	}
 #endif
 
-#ifdef CONFIG_PER_VMA_LOCK
 	if (!(flags & FAULT_FLAG_USER))
 		goto lock_mmap;
 
@@ -1359,7 +1358,6 @@ void do_user_addr_fault(struct pt_regs *regs,
 		return;
 	}
 lock_mmap:
-#endif /* CONFIG_PER_VMA_LOCK */
 
 retry:
 	vma = lock_mm_and_find_vma(mm, address, regs);
@@ -1419,9 +1417,7 @@ retry:
 	}
 
 	mmap_read_unlock(mm);
-#ifdef CONFIG_PER_VMA_LOCK
 done:
-#endif
 	if (likely(!(fault & VM_FAULT_ERROR)))
 		return;
 
