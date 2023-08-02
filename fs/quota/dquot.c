9543f84c067de36eae6e8dd53051837f7dabcdd7
Christoph Hellwig <hch@lst.de>
2023-08-02 13:54:38 +0200

    quota: mark dquot_load_quota_sb static
    dquot_load_quota_sb is only used in dquot.c, so mark it static.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Message-Id: <20230802115439.2145212-1-hch@lst.de>

--- fs/quota/dquot.c
+++ fs/quota/dquot.c
@@ -2358,8 +2358,8 @@ static int vfs_setup_quota_inode(struct inode *inode, int type)
 	return 0;
 }
 
-int dquot_load_quota_sb(struct super_block *sb, int type, int format_id,
-	unsigned int flags)
+static int dquot_load_quota_sb(struct super_block *sb, int type, int format_id,
+		unsigned int flags)
 {
 	struct quota_format_type *fmt = find_quota_format(format_id);
 	struct quota_info *dqopt = sb_dqopt(sb);
@@ -2435,7 +2435,6 @@ out_fmt:
 
 	return error;
 }
-EXPORT_SYMBOL(dquot_load_quota_sb);
 
 /*
  * More powerful function for turning on quotas on given quota inode allowing
