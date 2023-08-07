458497cea9ae8ee385afc9dd07fa6ff086c0be98
Peter Zijlstra <peterz@infradead.org>
2023-08-07 14:18:47 +0200

    futex: Validate futex value against futex size
    Ensure the futex value fits in the given futex size. Since this adds a
    constraint to an existing syscall, it might possibly change behaviour.
    
    Currently the value would be truncated to a u32 and any high bits
    would get silently lost.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- kernel/futex/futex.h
+++ kernel/futex/futex.h
@@ -85,6 +85,16 @@ static inline unsigned int futex_size(unsigned int flags)
 	return 1 << (flags & FLAGS_SIZE_MASK);
 }
 
+static inline bool futex_validate_input(unsigned int flags, u64 val)
+{
+	int bits = 8 * futex_size(flags);
+
+	if (bits < 64 && (val >> bits))
+		return false;
+
+	return true;
+}
+
 #ifdef CONFIG_FAIL_FUTEX
 extern bool should_fail_futex(bool fshared);
 #else
