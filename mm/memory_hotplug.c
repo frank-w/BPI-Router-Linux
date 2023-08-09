c3a0af7260232b4e33670765c9d7180ab2d4e252
Yang Yingliang <yangyingliang@huawei.com>
2023-08-09 16:15:52 +0800

    mm/memory_hotplug: fix error return code in add_memory_resource()
    Set error return code to -ENOMEM, when kmalloc() fails.
    
    Link: https://lkml.kernel.org/r/20230809081552.1351184-1-yangyingliang@huawei.com
    Fixes: ad3232df3e41 ("mm/memory_hotplug: embed vmem_altmap details in memory block")
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Reviewed-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -1440,8 +1440,10 @@ int __ref add_memory_resource(int nid, struct resource *res, mhp_t mhp_flags)
 		if (mhp_supports_memmap_on_memory(size)) {
 			mhp_altmap.free = memory_block_memmap_on_memory_pages();
 			params.altmap = kmalloc(sizeof(struct vmem_altmap), GFP_KERNEL);
-			if (!params.altmap)
+			if (!params.altmap) {
+				ret = -ENOMEM;
 				goto error;
+			}
 
 			memcpy(params.altmap, &mhp_altmap, sizeof(mhp_altmap));
 		}
