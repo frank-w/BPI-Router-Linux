d060eeb7456f84abdd298a9b76e9dfad7ff08fe4
Christoph Hellwig <hch@lst.de>
2023-08-11 12:08:14 +0200

    btrfs: call btrfs_close_devices from ->kill_sb
    blkdev_put must not be called under sb->s_umount to avoid a lock order
    reversal with disk->open_mutex once call backs from block devices to
    the file system using the holder ops are supported.  Move the call
    to btrfs_close_devices into btrfs_free_fs_info so that it is closed
    from ->kill_sb (which is also called from the mount failure handling
    path unlike ->put_super) as well as when an fs_info is freed because
    an existing superblock already exists.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Christian Brauner <brauner@kernel.org>
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Message-Id: <20230811100828.1897174-4-hch@lst.de>
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -1494,7 +1494,7 @@ static struct dentry *btrfs_mount_root(struct file_system_type *fs_type,
 
 	if (!(flags & SB_RDONLY) && fs_devices->rw_devices == 0) {
 		error = -EACCES;
-		goto error_close_devices;
+		goto error_fs_info;
 	}
 
 	bdev = fs_devices->latest_dev->bdev;
@@ -1502,11 +1502,10 @@ static struct dentry *btrfs_mount_root(struct file_system_type *fs_type,
 		 fs_info);
 	if (IS_ERR(s)) {
 		error = PTR_ERR(s);
-		goto error_close_devices;
+		goto error_fs_info;
 	}
 
 	if (s->s_root) {
-		btrfs_close_devices(fs_devices);
 		btrfs_free_fs_info(fs_info);
 		if ((flags ^ s->s_flags) & SB_RDONLY)
 			error = -EBUSY;
@@ -1527,8 +1526,6 @@ static struct dentry *btrfs_mount_root(struct file_system_type *fs_type,
 
 	return dget(s->s_root);
 
-error_close_devices:
-	btrfs_close_devices(fs_devices);
 error_fs_info:
 	btrfs_free_fs_info(fs_info);
 error_sec_opts:
