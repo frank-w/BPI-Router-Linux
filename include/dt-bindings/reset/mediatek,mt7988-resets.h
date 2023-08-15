ce3f768eb53621708f4bc9ee1bcf3f4170398749
Daniel Golle <daniel@makrotopia.org>
2023-08-15 01:15:24 +0100

    watchdog: mediatek: mt7988: add wdt support
    Add support for watchdog and reset generator unit unit of the MediaTek
    MT7988 SoC.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

---
+++ include/dt-bindings/reset/mediatek,mt7988-resets.h
@@ -0,0 +1,10 @@
+/* TOPRGU resets */
+#define MT7988_TOPRGU_SGMII0_GRST               1
+#define MT7988_TOPRGU_SGMII1_GRST               2
+#define MT7988_TOPRGU_XFI0_GRST                 12
+#define MT7988_TOPRGU_XFI1_GRST                 13
+#define MT7988_TOPRGU_XFI_PEXTP0_GRST           14
+#define MT7988_TOPRGU_XFI_PEXTP1_GRST           15
+#define MT7988_TOPRGU_XFI_PLL_GRST              16
+
+#define MT7988_TOPRGU_SW_RST_NUM                24
