72b150c587a021badedb3e962c30a525957875c8
Eric Woudstra <ericwouds@gmail.com>
2023-08-22 20:45:02 +0200

    net: phy: realtek: rtlgen_get_speed(): Pass register value as argument
    The register that needs to be read to determine the speed, is not
    always read in the same manner, also not always at the same address.
    
    To be able to use the one rtlgen_get_speed() function, generalize
    rtlgen_get_speed() even more. Pass the value from the device specific
    register as argument to rtlgen_get_speed().

--- drivers/net/phy/realtek.c
+++ drivers/net/phy/realtek.c
@@ -554,16 +554,10 @@ static int rtl8366rb_config_init(struct phy_device *phydev)
 }
 
 /* get actual speed to cover the downshift case */
-static int rtlgen_get_speed(struct phy_device *phydev)
+static int rtlgen_get_speed(struct phy_device *phydev, int val)
 {
-	int val;
-
 	if (!phydev->link)
 		return 0;
-
-	val = phy_read_paged(phydev, 0xa43, 0x12);
-	if (val < 0)
-		return val;
 
 	switch (val & RTLGEN_SPEED_MASK) {
 	case 0x0000:
@@ -593,13 +587,17 @@ static int rtlgen_get_speed(struct phy_device *phydev)
 
 static int rtlgen_read_status(struct phy_device *phydev)
 {
-	int ret;
+	int ret, val;
 
 	ret = genphy_read_status(phydev);
 	if (ret < 0)
 		return ret;
 
-	return rtlgen_get_speed(phydev);
+	val = phy_read_paged(phydev, 0xa43, 0x12);
+	if (val < 0)
+		return val;
+
+	return rtlgen_get_speed(phydev, val);
 }
 
 static int rtlgen_read_mmd(struct phy_device *phydev, int devnum, u16 regnum)
