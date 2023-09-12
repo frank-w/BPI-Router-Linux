koverstreet/bcachefs

bcachefs: added lock held time stats
We now record the length of time btree locks are held and expose this in debugfs.

Enabled via CONFIG_BCACHEFS_LOCK_TIME_STATS.

Signed-off-by: Daniel Hill <daniel@gluo.nz>

fs/bcachefs/bcachefs.h
@@ -319,6 +319,8 @@ BCH_DEBUG_PARAMS_DEBUG()
 #undef BCH_DEBUG_PARAM
 #endif
 
+#define BCH_LOCK_TIME_NR 128
+
 #define BCH_TIME_STATS()			\
 	x(btree_node_mem_alloc)			\
 	x(btree_node_split)			\
@@ -526,6 +528,11 @@ enum {
 
 struct btree_debug {
 	unsigned		id;
+};
+
+struct lock_held_stats {
+	struct time_stats       times[BCH_LOCK_TIME_NR];
+	const char              *names[BCH_LOCK_TIME_NR];
 };
 
 struct bch_fs_pcpu {
@@ -921,6 +928,8 @@ struct bch_fs {
 	bool			promote_whole_extents;
 
 	struct time_stats	times[BCH_TIME_STAT_NR];
+
+	struct lock_held_stats lock_held_stats;
 };
 
 static inline void bch2_set_ra_pages(struct bch_fs *c, unsigned ra_pages)

frank-w/BPI-Router-Linux

Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

fs/bcachefs/bcachefs.h
@@ -319,6 +319,8 @@ BCH_DEBUG_PARAMS_DEBUG()
 #undef BCH_DEBUG_PARAM
 #endif
 
+#define BCH_LOCK_TIME_NR 128
+
 #define BCH_TIME_STATS()			\
 	x(btree_node_mem_alloc)			\
 	x(btree_node_split)			\
@@ -525,6 +527,11 @@ enum {
 
 struct btree_debug {
 	unsigned		id;
+};
+
+struct lock_held_stats {
+	struct bch2_time_stats	times[BCH_LOCK_TIME_NR];
+	const char		*names[BCH_LOCK_TIME_NR];
 };
 
 struct bch_fs_pcpu {
@@ -920,6 +927,8 @@ mempool_t		bio_bounce_pages;
 	bool			promote_whole_extents;
 
 	struct bch2_time_stats	times[BCH_TIME_STAT_NR];
+
+	struct lock_held_stats	lock_held_stats;
 };
 
 static inline void bch2_set_ra_pages(struct bch_fs *c, unsigned ra_pages)