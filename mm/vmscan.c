eb421e8d772226cd32826259b15ce1d31e26550a
Dmitry Rokosov <ddrokosov@salutedevices.com>
2023-11-23 22:39:36 +0300

    mm: memcg: print out cgroup ino in the memcg tracepoints
    Patch series "mm: memcg: improve vmscan tracepoints", v3.
    
    The motivation behind this commit is to enhance the traceability and
    understanding of memcg events.  By integrating the function cgroup_ino()
    into the existing memcg tracepoints, this patch series introduces a new
    tracepoint template for the begin() and end() events.  It utilizes a new
    entry field ino to store the cgroup ino, enabling developers to easily
    identify the cgroup associated with a specific memcg tracepoint event. 
    
    Additionally, this patch series introduces new shrink_memcg tracepoints to
    facilitate non-direct memcg reclaim tracing and debugging.  
    
    
    This patch (of 2):
    
    Sometimes, it becomes necessary to determine the memcg tracepoint event
    that has occurred.  This is particularly relevant in scenarios involving a
    large cgroup hierarchy, where users may wish to trace the process of
    reclamation within a specific cgroup(s) by applying a filter.
    
    The function cgroup_ino() is a useful tool for this purpose.  To integrate
    cgroup_ino() into the existing memcg tracepoints, this patch introduces a
    new tracepoint template for the begin() and end() events.
    
    Link: https://lkml.kernel.org/r/20231123193937.11628-1-ddrokosov@salutedevices.com
    Link: https://lkml.kernel.org/r/20231123193937.11628-2-ddrokosov@salutedevices.com
    Signed-off-by: Dmitry Rokosov <ddrokosov@salutedevices.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Shakeel Butt <shakeelb@google.com>
    Cc: Steven Rostedt (Google) <rostedt@goodmis.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -6415,8 +6415,8 @@ unsigned long mem_cgroup_shrink_node(struct mem_cgroup *memcg,
 	sc.gfp_mask = (gfp_mask & GFP_RECLAIM_MASK) |
 			(GFP_HIGHUSER_MOVABLE & ~GFP_RECLAIM_MASK);
 
-	trace_mm_vmscan_memcg_softlimit_reclaim_begin(sc.order,
-						      sc.gfp_mask);
+	trace_mm_vmscan_memcg_softlimit_reclaim_begin(sc.order, sc.gfp_mask,
+						      memcg);
 
 	/*
 	 * NOTE: Although we can get the priority field, using it
@@ -6427,7 +6427,7 @@ unsigned long mem_cgroup_shrink_node(struct mem_cgroup *memcg,
 	 */
 	shrink_lruvec(lruvec, &sc);
 
-	trace_mm_vmscan_memcg_softlimit_reclaim_end(sc.nr_reclaimed);
+	trace_mm_vmscan_memcg_softlimit_reclaim_end(sc.nr_reclaimed, memcg);
 
 	*nr_scanned = sc.nr_scanned;
 
@@ -6461,13 +6461,13 @@ unsigned long try_to_free_mem_cgroup_pages(struct mem_cgroup *memcg,
 	struct zonelist *zonelist = node_zonelist(numa_node_id(), sc.gfp_mask);
 
 	set_task_reclaim_state(current, &sc.reclaim_state);
-	trace_mm_vmscan_memcg_reclaim_begin(0, sc.gfp_mask);
+	trace_mm_vmscan_memcg_reclaim_begin(0, sc.gfp_mask, memcg);
 	noreclaim_flag = memalloc_noreclaim_save();
 
 	nr_reclaimed = do_try_to_free_pages(zonelist, &sc);
 
 	memalloc_noreclaim_restore(noreclaim_flag);
-	trace_mm_vmscan_memcg_reclaim_end(nr_reclaimed);
+	trace_mm_vmscan_memcg_reclaim_end(nr_reclaimed, memcg);
 	set_task_reclaim_state(current, NULL);
 
 	return nr_reclaimed;
