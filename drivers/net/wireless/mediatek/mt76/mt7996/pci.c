3bdca4f9bb1e10dba7919ef1afc1d59cbb7479e6
Frank Wunderlich <frank-w@public-files.de>
2023-11-30 22:07:37 +0100

    Revert "wifi: mt76: mt7996: add dma mask limitation"
    This reverts commit 820a2a242b73b854029abc85ab6cfe8327cc6d83.

--- drivers/net/wireless/mediatek/mt76/mt7996/pci.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/pci.c
@@ -107,7 +107,7 @@ static int mt7996_pci_probe(struct pci_dev *pdev,
 
 	pci_set_master(pdev);
 
-	ret = dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(32));
+	ret = dma_set_mask(&pdev->dev, DMA_BIT_MASK(32));
 	if (ret)
 		return ret;
 
