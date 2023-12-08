aa87ee7d157894ad6f3651f029cbe6f6aa9abebe
Günther Noack <gnoack@google.com>
2023-12-08 16:51:16 +0100

    landlock: Add IOCTL access right
    Introduces the LANDLOCK_ACCESS_FS_IOCTL access right
    and increments the Landlock ABI version to 5.
    
    Like the truncate right, these rights are associated with a file
    descriptor at the time of open(2), and get respected even when the
    file descriptor is used outside of the thread which it was originally
    opened in.
    
    A newly enabled Landlock policy therefore does not apply to file
    descriptors which are already open.
    
    If the LANDLOCK_ACCESS_FS_IOCTL right is handled, only a small number
    of safe IOCTL commands will be permitted on newly opened files.  The
    permitted IOCTLs can be configured through the ruleset in limited ways
    now.  (See documentation for details.)
    
    Specifically, when LANDLOCK_ACCESS_FS_IOCTL is handled, granting this
    right on a file or directory will *not* permit to do all IOCTL
    commands, but only influence the IOCTL commands which are not already
    handled through other access rights.  The intent is to keep the groups
    of IOCTL commands more fine-grained.
    
    Noteworthy scenarios which require special attention:
    
    TTY devices support IOCTLs like TIOCSTI and TIOCLINUX, which can be
    used to control shell processes on the same terminal which run at
    different privilege levels, which may make it possible to escape a
    sandbox.  Because stdin, stdout and stderr are normally inherited
    rather than newly opened, IOCTLs are usually permitted on them even
    after the Landlock policy is enforced.
    
    Some legitimate file system features, like setting up fscrypt, are
    exposed as IOCTL commands on regular files and directories -- users of
    Landlock are advised to double check that the sandboxed process does
    not need to invoke these IOCTLs.
    
    Known limitations:
    
    The LANDLOCK_ACCESS_FS_IOCTL access right is a coarse-grained control
    over IOCTL commands.  Future work will enable a more fine-grained
    access control for IOCTLs.
    
    In the meantime, Landlock users may use path-based restrictions in
    combination with their knowledge about the file system layout to
    control what IOCTLs can be done.  Mounting file systems with the nodev
    option can help to distinguish regular files and devices, and give
    guarantees about the affected files, which Landlock alone can not give
    yet.
    
    Signed-off-by: Günther Noack <gnoack@google.com>
    Link: https://lore.kernel.org/r/20231208155121.1943775-5-gnoack@google.com
    Signed-off-by: Mickaël Salaün <mic@digikod.net>

--- security/landlock/limits.h
+++ security/landlock/limits.h
@@ -18,7 +18,16 @@
 #define LANDLOCK_MAX_NUM_LAYERS		16
 #define LANDLOCK_MAX_NUM_RULES		U32_MAX
 
-#define LANDLOCK_LAST_ACCESS_FS		LANDLOCK_ACCESS_FS_TRUNCATE
+/*
+ * For file system access rights, Landlock distinguishes between the publicly
+ * visible access rights (1 to LANDLOCK_LAST_PUBLIC_ACCESS_FS) and the private
+ * ones which are not exposed to userspace (LANDLOCK_LAST_PUBLIC_ACCESS_FS + 1
+ * to LANDLOCK_LAST_ACCESS_FS).  The private access rights are defined in fs.c.
+ */
+#define LANDLOCK_LAST_PUBLIC_ACCESS_FS	LANDLOCK_ACCESS_FS_IOCTL
+#define LANDLOCK_MASK_PUBLIC_ACCESS_FS	((LANDLOCK_LAST_PUBLIC_ACCESS_FS << 1) - 1)
+
+#define LANDLOCK_LAST_ACCESS_FS		(LANDLOCK_LAST_PUBLIC_ACCESS_FS << 4)
 #define LANDLOCK_MASK_ACCESS_FS		((LANDLOCK_LAST_ACCESS_FS << 1) - 1)
 #define LANDLOCK_NUM_ACCESS_FS		__const_hweight64(LANDLOCK_MASK_ACCESS_FS)
 #define LANDLOCK_SHIFT_ACCESS_FS	0
