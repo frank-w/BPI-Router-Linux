743100b40e1eb92b88417f08c1fd335b24512433
Ryan Roberts <ryan.roberts@arm.com>
2023-12-11 12:53:20 +0000

    mm: thp: fix build warning when CONFIG_SYSFS is disabled
    huge_anon_orders_lock is used only to serialize sysfs writers.  So move
    its definition so that it is within the CONFIG_SYSFS ifdefery to suppress
    "defined but not used" warning when sysfs is disabled.
    
    Link: https://lkml.kernel.org/r/20231211125320.3997543-1-ryan.roberts@arm.com
    Fixes: ("mm: thp: Introduce multi-size THP sysfs interface")
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202312111916.YbsHxKPq-lkp@intel.com/
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -77,7 +77,6 @@ unsigned long huge_zero_pfn __read_mostly = ~0UL;
 unsigned long huge_anon_orders_always __read_mostly;
 unsigned long huge_anon_orders_madvise __read_mostly;
 unsigned long huge_anon_orders_inherit __read_mostly;
-static DEFINE_SPINLOCK(huge_anon_orders_lock);
 
 unsigned long __thp_vma_allowable_orders(struct vm_area_struct *vma,
 					 unsigned long vm_flags, bool smaps,
@@ -442,6 +441,7 @@ static const struct attribute_group hugepage_attr_group = {
 
 static void hugepage_exit_sysfs(struct kobject *hugepage_kobj);
 static void thpsize_release(struct kobject *kobj);
+static DEFINE_SPINLOCK(huge_anon_orders_lock);
 static LIST_HEAD(thpsize_list);
 
 struct thpsize {
