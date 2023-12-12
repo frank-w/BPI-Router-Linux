2cedc5c4cbed533323b2de3147dcae85d1b656f6
Yury Norov <yury.norov@gmail.com>
2023-12-11 18:27:31 -0800

    scsi: qedi: optimize qedi_get_task_idx() by using find_and_set_bit()
    qedi_get_task_idx() opencodes find_and_set_bit(). Simplify it and make the
    whole function a simiple almost one-liner.
    
    CC: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Yury Norov <yury.norov@gmail.com>

--- drivers/scsi/qedi/qedi_main.c
+++ drivers/scsi/qedi/qedi_main.c
@@ -1824,20 +1824,13 @@ int qedi_get_task_idx(struct qedi_ctx *qedi)
 {
 	s16 tmp_idx;
 
-again:
-	tmp_idx = find_first_zero_bit(qedi->task_idx_map,
-				      MAX_ISCSI_TASK_ENTRIES);
+	tmp_idx = find_and_set_bit(qedi->task_idx_map, MAX_ISCSI_TASK_ENTRIES);
 
 	if (tmp_idx >= MAX_ISCSI_TASK_ENTRIES) {
 		QEDI_ERR(&qedi->dbg_ctx, "FW task context pool is full.\n");
 		tmp_idx = -1;
-		goto err_idx;
 	}
 
-	if (test_and_set_bit(tmp_idx, qedi->task_idx_map))
-		goto again;
-
-err_idx:
 	return tmp_idx;
 }
 
