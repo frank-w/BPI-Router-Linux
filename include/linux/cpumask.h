cd6c08c6647d524d71f51428b78fe72590d42c16
Yury Norov <yury.norov@gmail.com>
2023-12-11 18:27:19 -0800

    sched: add cpumask_find_and_set() and use it in __mm_cid_get()
    __mm_cid_get() uses __mm_cid_try_get() helper to atomically acquire a
    bit in mm cid mask. Now that we have atomic find_and_set_bit(), we can
    easily extend it to cpumasks and use in the scheduler code.
    
    cpumask_find_and_set() considers cid mask as a volatile region of memory,
    as it actually is in this case. So, if it's changed while search is in
    progress, KCSAN wouldn't fire warning on it.
    
    CC: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Yury Norov <yury.norov@gmail.com>
    Reviewed-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -272,6 +272,18 @@ unsigned int cpumask_next_and(int n, const struct cpumask *src1p,
 }
 
 /**
+ * cpumask_find_and_set - find the first unset cpu in a cpumask and
+ *			  set it atomically
+ * @srcp: the cpumask pointer
+ *
+ * Return: >= nr_cpu_ids if nothing is found.
+ */
+static inline unsigned int cpumask_find_and_set(volatile struct cpumask *srcp)
+{
+	return find_and_set_bit(cpumask_bits(srcp), small_cpumask_bits);
+}
+
+/**
  * for_each_cpu - iterate over every cpu in a mask
  * @cpu: the (optionally unsigned) integer iterator
  * @mask: the cpumask pointer
