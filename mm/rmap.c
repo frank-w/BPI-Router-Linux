f5e47581c5fe07adfb1fbecf36d252e474992ded
Lance Yang <ioworker0@gmail.com>
2024-05-21 12:02:44 +0800

    mm/vmscan: avoid split lazyfree THP during shrink_folio_list()
    When the user no longer requires the pages, they would use
    madvise(MADV_FREE) to mark the pages as lazy free.  Subsequently, they
    typically would not re-write to that memory again.
    
    During memory reclaim, if we detect that the large folio and its PMD are
    both still marked as clean and there are no unexpected references (such as
    GUP), so we can just discard the memory lazily, improving the efficiency
    of memory reclamation in this case.
    
    On an Intel i5 CPU, reclaiming 1GiB of lazyfree THPs using
    mem_cgroup_force_empty() results in the following runtimes in seconds
    (shorter is better):
    
    --------------------------------------------
    |     Old       |      New       |  Change  |
    --------------------------------------------
    |   0.683426    |    0.049197    |  -92.80% |
    --------------------------------------------
    
    Link: https://lkml.kernel.org/r/20240521040244.48760-4-ioworker0@gmail.com
    Signed-off-by: Lance Yang <ioworker0@gmail.com>
    Suggested-by: Zi Yan <ziy@nvidia.com>
    Suggested-by: David Hildenbrand <david@redhat.com>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Barry Song <21cnbao@gmail.com>
    Cc: Fangrui Song <maskray@google.com>
    Cc: Jeff Xie <xiehuan09@gmail.com>
    Cc: Kefeng Wang <wangkefeng.wang@huawei.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Yang Shi <shy828301@gmail.com>
    Cc: Yin Fengwei <fengwei.yin@intel.com>
    Cc: Zach O'Keefe <zokeefe@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/rmap.c
+++ mm/rmap.c
@@ -1630,6 +1630,7 @@ static bool try_to_unmap_one(struct folio *folio, struct vm_area_struct *vma,
 	enum ttu_flags flags = (enum ttu_flags)(long)arg;
 	unsigned long pfn;
 	unsigned long hsz = 0;
+	bool pmd_mapped = false;
 
 	/*
 	 * When racing against e.g. zap_pte_range() on another cpu,
@@ -1677,18 +1678,26 @@ static bool try_to_unmap_one(struct folio *folio, struct vm_area_struct *vma,
 			goto walk_done_err;
 		}
 
-		if (!pvmw.pte && (flags & TTU_SPLIT_HUGE_PMD)) {
-			/*
-			 * We temporarily have to drop the PTL and start once
-			 * again from that now-PTE-mapped page table.
-			 */
-			split_huge_pmd_locked(vma, range.start, pvmw.pmd, false,
-					      folio);
-			pvmw.pmd = NULL;
-			spin_unlock(pvmw.ptl);
-			pvmw.ptl = NULL;
-			flags &= ~TTU_SPLIT_HUGE_PMD;
-			continue;
+		if (!pvmw.pte) {
+			pmd_mapped = true;
+			if (unmap_huge_pmd_locked(vma, range.start, pvmw.pmd,
+						  folio))
+				goto walk_done;
+
+			if (flags & TTU_SPLIT_HUGE_PMD) {
+				/*
+				 * We temporarily have to drop the PTL and start
+				 * once again from that now-PTE-mapped page
+				 * table.
+				 */
+				split_huge_pmd_locked(vma, range.start,
+						      pvmw.pmd, false, folio);
+				pvmw.pmd = NULL;
+				spin_unlock(pvmw.ptl);
+				pvmw.ptl = NULL;
+				flags &= ~TTU_SPLIT_HUGE_PMD;
+				continue;
+			}
 		}
 
 		/* Unexpected PMD-mapped THP? */
@@ -1816,7 +1825,13 @@ static bool try_to_unmap_one(struct folio *folio, struct vm_area_struct *vma,
 			 */
 			if (unlikely(folio_test_swapbacked(folio) !=
 					folio_test_swapcache(folio))) {
-				WARN_ON_ONCE(1);
+				/*
+				 * unmap_huge_pmd_locked() will unmark a
+				 * PMD-mapped folio as lazyfree if the folio or
+				 * its PMD was redirtied.
+				 */
+				if (!pmd_mapped)
+					WARN_ON_ONCE(1);
 				goto walk_done_err;
 			}
 
