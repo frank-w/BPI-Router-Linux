041a7cceecf2e6e2bda30c572e2ba265eca6c4a9
Frank Wunderlich <frank-w@public-files.de>
2024-08-30 19:20:45 +0200

    utils: docker: run container in priviliged mode for mount

--- utils/docker/run.sh
+++ utils/docker/run.sh
@@ -4,4 +4,4 @@ DIR=$(dirname "$(readlink -f "$0")")
 REPO_DIR="$DIR/../.."
 
 mkdir -p "$REPO_DIR/SD"
-docker run -it -v "$REPO_DIR/SD":/SD -v "$REPO_DIR":/srv bpi-cross-compile:1 /bin/bash
+docker run --privileged -it -v "$REPO_DIR/SD":/SD -v "$REPO_DIR":/srv bpi-cross-compile:1 /bin/bash
