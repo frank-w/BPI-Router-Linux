c38087820118e4bd0ac9c5d756d242c13ffd2ce4
Frank Wunderlich <frank-w@public-files.de>
2024-08-31 11:28:35 +0200

    utils: docker: fixed some problems
    - out-of-tree (../build) working
    - ramdisk working
    - move package in kerneldir/SD
    - building debs (here target is .. and need to be manually copied to SD-folder)
      i have not found way to move them without touching build.sh

--- utils/docker/run.sh
+++ utils/docker/run.sh
@@ -3,5 +3,12 @@
 DIR=$(dirname "$(readlink -f "$0")")
 REPO_DIR="$DIR/../.."
 
+export UID=$(id -u)
+export GID=$(id -g)
+
 mkdir -p "$REPO_DIR/SD"
-docker run --privileged -it -v "$REPO_DIR/SD":/SD -v "$REPO_DIR":/srv bpi-cross-compile:1 /bin/bash
+docker run --privileged -it \
+	--user $UID:$GID \
+	-v "$REPO_DIR/SD":/srv/SD \
+	-v "$REPO_DIR":/srv/code \
+	bpi-cross-compile:1 /bin/bash
