b4e835e7effb778d4108e9a343ff47a7d80957f0
Christian Marangi <ansuelsmth@gmail.com>
2025-04-07 00:14:00 +0200

    net: phylink: support late PCS provider attach
    Add support in phylink for late PCS provider attach to a phylink
    instance. This works by creating a global notifier for the PCS provider
    and making each phylink instance that makes use of fwnode subscribe to
    this notifier.
    
    The PCS notifier will emit the event FWNODE_PCS_PROVIDER_ADD every time
    a new PCS provider is added.
    
    phylink will then react to this event and will call the new function
    fwnode_phylink_pcs_get_from_fwnode() that will check if the PCS fwnode
    provided by the event is present in the phy-handle property of the
    phylink instance.
    
    If a related PCS is found, then such PCS is added to the phylink
    instance PCS list.
    
    Then we link the PCS to the phylink instance if it's not disable and we
    refresh the supported interfaces of the phylink instance.
    
    Finally we check if we are in a major_config_failed scenario and trigger
    an interface reconfiguration in the next phylink resolve.
    
    If link was previously torn down due to removal of PCS, the link will be
    established again as the PCS came back and is not available to phylink.
    
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

--- include/linux/pcs/pcs.h
+++ include/linux/pcs/pcs.h
@@ -4,7 +4,24 @@
 
 #include <linux/phylink.h>
 
+enum fwnode_pcs_notify_event {
+	FWNODE_PCS_PROVIDER_ADD,
+};
+
 #if IS_ENABLED(CONFIG_FWNODE_PCS)
+/**
+ * register_fwnode_pcs_notifier - Register a notifier block for fwnode
+ *				  PCS events
+ * @nb: pointer to the notifier block
+ *
+ * Registers a notifier block to the fwnode_pcs_notify_list blocking
+ * notifier chain. This allows phylink instance to subscribe for
+ * PCS provider events.
+ *
+ * Returns 0 or a negative error.
+ */
+int register_fwnode_pcs_notifier(struct notifier_block *nb);
+
 /**
  * fwnode_pcs_get - Retrieves a PCS from a firmware node
  * @fwnode: firmware node
@@ -19,6 +36,25 @@
  */
 struct phylink_pcs *fwnode_pcs_get(struct fwnode_handle *fwnode,
 				   int index);
+
+/**
+ * fwnode_phylink_pcs_get_from_fwnode - Retrieves the PCS provided
+ *					by the firmware node from a
+ *					firmware node
+ * @fwnode: firmware node
+ * @pcs_fwnode: PCS firmware node
+ *
+ * Parse 'pcs-handle' in 'fwnode' and get the PCS that match
+ * 'pcs_fwnode' firmware node.
+ *
+ * Returns a pointer to the phylink_pcs or a negative
+ * error pointer. Can return -EPROBE_DEFER if the PCS is not
+ * present in global providers list (either due to driver
+ * still needs to be probed or it failed to probe/removed)
+ */
+struct phylink_pcs *
+fwnode_phylink_pcs_get_from_fwnode(struct fwnode_handle *fwnode,
+				   struct fwnode_handle *pcs_fwnode);
 
 /**
  * fwnode_phylink_pcs_parse - generic PCS parse for fwnode PCS provider
@@ -39,8 +75,20 @@ int fwnode_phylink_pcs_parse(struct fwnode_handle *fwnode,
 			     struct phylink_pcs **available_pcs,
 			     unsigned int *num_pcs);
 #else
+static int register_fwnode_pcs_notifier(struct notifier_block *nb)
+{
+	return -EOPNOTSUPP;
+}
+
 static inline struct phylink_pcs *fwnode_pcs_get(struct fwnode_handle *fwnode,
 						 int index)
+{
+	return ERR_PTR(-ENOENT);
+}
+
+static struct phylink_pcs *
+fwnode_phylink_pcs_get_from_fwnode(struct fwnode_handle *fwnode,
+				   struct fwnode_handle *pcs_fwnode)
 {
 	return ERR_PTR(-ENOENT);
 }
