6751c195702add17a21cf5778d8cdda448786a07
Christian Marangi <ansuelsmth@gmail.com>
2025-04-07 00:14:02 +0200

    net: pcs: airoha: add PCS driver for Airoha SoC
    Add PCS driver for Airoha SoC for Ethernet Serdes and PCS and permit
    usage of external PHY or connected SFP cage. Supported modes are
    USXGMII, 10-BASER, 2500BASE-X, 1000BASE-X and SGMII.
    
    The driver register as a PCS provider and supports poll mode to detect
    PCS port state. While interrupt is supported by the HW, there are some
    defect with the interrupt not fired on cable detach.
    
    The PCS require complex calibration to correctly work and might require
    multiple try until the signal detection module "lock" on the signal
    level to correct work with the attached PHY.
    
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

---
+++ include/linux/pcs/pcs-airoha.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __LINUX_PCS_AIROHA_H
+#define __LINUX_PCS_AIROHA_H
+
+/* XFI_MAC */
+#define AIROHA_PCS_XFI_MAC_XFI_GIB_CFG		0x0
+#define   AIROHA_PCS_XFI_TX_FC_EN		BIT(5)
+#define   AIROHA_PCS_XFI_RX_FC_EN		BIT(4)
+
+#endif /* __LINUX_PCS_AIROHA_H */
