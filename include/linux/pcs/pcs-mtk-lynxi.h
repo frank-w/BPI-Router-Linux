188fbd9a282fbed6a7a43430ab5d611dacfee1c5
Bo-Cun Chen <bc-bocun.chen@mediatek.com>
2025-05-06 12:53:37 +0800

    net: pcs: mtk-lynxi: add phya tx rx clock path
    In NETSYSv3.1, the SGMII hardware introduces a new clock path from PHYA.
    Consequently, users can switch the SGMII PCS to this new clock source
    for better performance on the MT7987.
    
    Signed-off-by: Bo-Cun Chen <bc-bocun.chen@mediatek.com>

--- include/linux/pcs/pcs-mtk-lynxi.h
+++ include/linux/pcs/pcs-mtk-lynxi.h
@@ -5,6 +5,8 @@
 #include <linux/phylink.h>
 #include <linux/regmap.h>
 
+#define MTK_SGMII_FLAG_PHYA_TRX_CK BIT(2)
+
 struct phylink_pcs *mtk_pcs_lynxi_create(struct device *dev,
 					 struct fwnode_handle *fwnode,
 					 struct regmap *regmap, u32 ana_rgc3);
