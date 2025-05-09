c20c9ad5e80816ee513e0466516c3016ffb8bf28
Christian Marangi <ansuelsmth@gmail.com>
2025-05-09 16:36:22 +0200

    net: phylink: add .pcs_link_down PCS OP
    Permit for PCS driver to define specific operation to torn down the link
    between the MAC and the PCS.
    
    This might be needed for some PCS that reset counter or require special
    reset to correctly work if the link needs to be restored later.
    
    On phylink_link_down() call, the additional phylink_pcs_link_down() will
    be called before .mac_link_down to torn down the link.
    
    PCS driver will need to define .pcs_link_down to make use of this.
    
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

--- include/linux/phylink.h
+++ include/linux/phylink.h
@@ -523,6 +523,7 @@ struct phylink_pcs {
  * @pcs_an_restart: restart 802.3z BaseX autonegotiation.
  * @pcs_link_up: program the PCS for the resolved link configuration
  *               (where necessary).
+ * @pcs_link_down: torn down link between MAC and PCS.
  * @pcs_disable_eee: optional notification to PCS that EEE has been disabled
  *		     at the MAC.
  * @pcs_enable_eee: optional notification to PCS that EEE will be enabled at
@@ -550,6 +551,7 @@ struct phylink_pcs_ops {
 	void (*pcs_an_restart)(struct phylink_pcs *pcs);
 	void (*pcs_link_up)(struct phylink_pcs *pcs, unsigned int neg_mode,
 			    phy_interface_t interface, int speed, int duplex);
+	void (*pcs_link_down)(struct phylink_pcs *pcs);
 	void (*pcs_disable_eee)(struct phylink_pcs *pcs);
 	void (*pcs_enable_eee)(struct phylink_pcs *pcs);
 	int (*pcs_pre_init)(struct phylink_pcs *pcs);
