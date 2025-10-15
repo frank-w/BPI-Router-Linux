b089d81aafcfc7ee1158b2d3e83a92d235cb6d7d
Frank Wunderlich <frank-w@public-files.de>
2025-10-15 20:50:19 +0200

    net: phy: as21xxx: drop 2 debugs

--- drivers/net/phy/as21xxx.c
+++ drivers/net/phy/as21xxx.c
@@ -784,7 +784,6 @@ static int as21xxx_probe(struct phy_device *phydev)
 	struct as21xxx_priv *priv;
 	int ret;
 
-	phydev_err(phydev, "DEBUG %s:%d\n", __func__,__LINE__);
 	priv = devm_kzalloc(&phydev->mdio.dev,
 			    sizeof(*priv), GFP_KERNEL);
 	if (!priv)
@@ -964,7 +963,6 @@ static int as21xxx_read_status(struct phy_device *phydev)
 		aeon_read_speed(phydev);
 	}
 
-	phydev_err(phydev, "DEBUG %s:%d\n", __func__,__LINE__);
 	return 0;
 }
 
