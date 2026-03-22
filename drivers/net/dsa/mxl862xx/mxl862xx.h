76a7b10c5af8c4a30ffe3e47c3c3fdb3bff9fcf3
Frank Wunderlich <frank-w@public-files.de>
2026-03-22 14:56:05 +0100

    Revert "net: dsa: mxl862xx: implement bridge offloading"
    This reverts commit 2237fdb697bfda292a913d6126a5fd406e29d5a0.

--- drivers/net/dsa/mxl862xx/mxl862xx.h
+++ drivers/net/dsa/mxl862xx/mxl862xx.h
@@ -7,31 +7,10 @@
 #include <net/dsa.h>
 
 #define MXL862XX_MAX_PORTS		17
-#define MXL862XX_DEFAULT_BRIDGE		0
-#define MXL862XX_MAX_BRIDGES		48
-#define MXL862XX_MAX_BRIDGE_PORTS	128
-
-struct mxl862xx_bridge {
-	unsigned int dsa_bridge_num;
-	u16 bridge_id;
-	DECLARE_BITMAP(portmap, MXL862XX_MAX_BRIDGE_PORTS);
-	struct list_head list;
-};
-
-struct mxl862xx_port {
-	u16 fid; /* single-port bridge ID (permanent) */
-	struct mxl862xx_bridge *bridge;
-	DECLARE_BITMAP(portmap, MXL862XX_MAX_BRIDGE_PORTS);
-	unsigned long flood_block; /* bitmask of meter indices with metering on */
-	bool learning;
-};
 
 struct mxl862xx_priv {
 	struct dsa_switch *ds;
 	struct mdio_device *mdiodev;
-	u16 drop_meter; /* single zero-rate meter shared by all ports */
-	struct mxl862xx_port ports[MXL862XX_MAX_PORTS];
-	struct list_head bridges;
 };
 
 #endif /* __MXL862XX_H */
