a74ee1160b8fb066392a1fbbc042bee85f049518
Daniel Golle <daniel@makrotopia.org>
2026-03-24 16:30:17 +0000

    net: dsa: mxl862xx: add SMDIO clause-22 register access
    Add mxl862xx_smdio_read() and mxl862xx_smdio_write() for clause-22
    SMDIO register access. MCUboot rescue mode only exposes clause-22
    registers; the existing clause-45 MMD interface is unavailable during
    firmware transfer. The MDIO bus lock is held per-transaction (not
    across polls) so that SB PDI polling during flash erase does not
    starve other MDIO users.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- drivers/net/dsa/mxl862xx/mxl862xx-host.h
+++ drivers/net/dsa/mxl862xx/mxl862xx-host.h
@@ -10,5 +10,7 @@ void mxl862xx_host_shutdown(struct mxl862xx_priv *priv);
 int mxl862xx_api_wrap(struct mxl862xx_priv *priv, u16 cmd, void *data, u16 size,
 		      bool read, bool quiet);
 int mxl862xx_reset(struct mxl862xx_priv *priv);
+int mxl862xx_smdio_read(struct mxl862xx_priv *priv, u32 addr);
+int mxl862xx_smdio_write(struct mxl862xx_priv *priv, u32 addr, u16 val);
 
 #endif /* __MXL862XX_HOST_H */
