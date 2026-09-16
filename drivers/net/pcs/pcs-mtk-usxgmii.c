e69eb61a1523c5e993803c05a42c55c7576b07d3
Bo-Cun Chen <bc-bocun.chen@mediatek.com>
2026-09-16 20:50:24 +0200

    net: pcs: mtk-usxgmii: fix usxgmii link flapping the mtk way
    For the USXGMII link‑flap issue, remove statements that are already
    executed inside mtk_usxgmii_pcs_config().

--- drivers/net/pcs/pcs-mtk-usxgmii.c
+++ drivers/net/pcs/pcs-mtk-usxgmii.c
@@ -321,8 +321,6 @@ static void mtk_usxgmii_pcs_link_up(struct phylink_pcs *pcs, unsigned int neg_mo
 	 * after the line side link up.
 	 */
 	mtk_usxgmii_pcs_config(pcs, neg_mode, interface, NULL, false);
-	phy_reset(mpcs->xfi_tphy);
-	phy_set_mode_ext(mpcs->xfi_tphy, PHY_MODE_ETHERNET, interface);
 }
 
 static int mtk_usxgmii_pcs_enable(struct phylink_pcs *pcs)
